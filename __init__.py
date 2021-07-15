b9d8207bd8ac76abefdc6a34e7c7e44fe45e4e26
iamkroot <kroot.patel@gmail.com>
2021-07-15 15:11:22 +0530

    logging: Only log to stderr in DEBUG mode

--- __init__.py
+++ __init__.py
@@ -17,10 +17,13 @@ logging.addLevelName(logging.TRACE, "trace")
 logging.trace = lambda *args, **kwargs: logging.log(5, *args, **kwargs)
 LOG_FILE = "cowin_booking.log"
 
-stderr_handler = logging.StreamHandler()
 file_handler = logging.FileHandler(LOG_FILE, mode="a")
+handlers: list[logging.Handler] = [file_handler]
+if DEBUG:
+    stderr_handler = logging.StreamHandler()
+    handlers.append(stderr_handler)
 logging.basicConfig(
-    handlers=[stderr_handler, file_handler],
+    handlers=handlers,
     level=logging.TRACE if DEBUG else logging.DEBUG,  # more verbose if debugging
     format="{asctime} - {levelname} - {message}",
     style="{",
